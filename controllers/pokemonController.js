1da338c7a661c7dba75d57893dd1fc477400c785
Cristhian Braun <114692497+CBGMDias@users.noreply.github.com>
2024-10-18 19:37:23 -0300

    Adicionado as rotas e views
    Views:
    Pokedex - index.ejs
    Pokemon - pokemon.ejs
    
    Routes:
    getAllPokemons
    getPokemon

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,17 +1,17 @@
-const pokemonModel = require('../models/pokemonModel');
+const pokemonModel = require('../models/pokemonModel')
 
 const getAllPokemons = (req, res) => {
-    const pokemons = pokemonModel.getPokemons();
-    res.render('index', { pokemons });
+    const pokemons = pokemonModel.getPokemons()
+    res.render('index', { pokemons })
 };
 
 const getPokemon = (req, res) => {
-    const pokemon = pokemonModel.getPokemonById(req.params.id);
+    const pokemon = pokemonModel.getPokemonById(req.params.id)
     if (pokemon) {
-        res.render('pokemon', { pokemon });
+        res.render('pokemon', { pokemon })
     } else {
-        res.status(404).send('Pokémon não encontrado');
+        res.status(404).send('Pokémon não encontrado')
     }
 };
 
-module.exports={ getAllPokemons, getPokemon };
+module.exports={ getAllPokemons, getPokemon }
